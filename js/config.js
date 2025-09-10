ee6d2c63c82d15ef0412ebd1fda1ced65677fb9e
PrasadHM-ctr <prasadhm439@gmail.com>
2025-09-10 21:39:47 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_kKFDWi2T4', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '229c9hqgg6pfa13mkc0e2a8mlq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_bjgZa0aJq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3rh1cfsoo38bbeoadm444333jm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
